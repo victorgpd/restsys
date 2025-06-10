93d4b03a5a4e7905adc556017cee90262f9334e9
victorgpd <victorguilherme2407@outlook.com>
2025-06-10 19:32:53 -0300

    inserindo componentes no header

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,8 +1,9 @@
 import { Button } from "antd";
-import { HeaderContainer } from "./styles";
-import { MenuOutlined } from "@ant-design/icons";
+import { HeaderContainer, HeaderContainerButtons, HeaderLinkLogo, HeaderLogoContainer, HeaderLogotipo, HeaderTitle } from "./styles";
+import { CloseOutlined, MenuOutlined } from "@ant-design/icons";
 import { useAppDispatch, useAppSelector } from "../../utils/useStore";
 import { setMenuIsOpen } from "../../redux/globalReducer/slice";
+import { RoutesEnums } from "../../types/enums";
 
 const Header = () => {
   const dispatch = useAppDispatch();
@@ -14,7 +15,37 @@ const Header = () => {
 
   return (
     <HeaderContainer style={window.location.pathname.includes("dashboard") ? { width: menuIsOpen ? "calc(100% - 246px)" : "100%", left: menuIsOpen ? 246 : 0 } : { width: "100%", left: 0 }}>
-      {window.location.pathname.includes("dashboard") && <Button type="text" onClick={handleToggleMenu} icon={<MenuOutlined style={{ color: "#fff" }} />} style={{ fontSize: "1.5rem" }} />}
+      <HeaderLogoContainer>
+        {window.location.pathname.includes("dashboard") && (
+          <Button type="text" onClick={handleToggleMenu} icon={menuIsOpen ? <CloseOutlined style={{ color: "#fff" }} /> : <MenuOutlined style={{ color: "#fff" }} />} style={{ fontSize: "1.5rem" }} />
+        )}
+        <HeaderLinkLogo href="/">
+          <HeaderLogotipo src="/images/logo-vertical.png" alt="Logotipo" />
+          <HeaderTitle>ResSys</HeaderTitle>
+        </HeaderLinkLogo>
+      </HeaderLogoContainer>
+
+      <HeaderContainerButtons>
+        <Button
+          type="link"
+          variant="link"
+          style={{ color: "#fff" }}
+          onClick={() => {
+            window.location.href = RoutesEnums.Login;
+          }}
+        >
+          Entrar
+        </Button>
+        <Button
+          variant="solid"
+          color="blue"
+          onClick={() => {
+            window.location.href = RoutesEnums.Register;
+          }}
+        >
+          Cadastrar
+        </Button>
+      </HeaderContainerButtons>
     </HeaderContainer>
   );
 };
--- src/components/Header/styles.ts
+++ src/components/Header/styles.ts
@@ -3,7 +3,7 @@ import styled from "styled-components";
 export const HeaderContainer = styled.header`
   width: calc(100% - 246px);
   height: 70px;
-  padding: 16px 24px;
+  padding: 10px 24px;
 
   color: #ffffff;
   background-color: #333333;
@@ -20,4 +20,35 @@ export const HeaderContainer = styled.header`
   z-index: 1;
 
   transition: left 0.3s ease-in-out, width 0.3s ease-in-out;
+`;
+
+export const HeaderLogoContainer = styled.div`
+  height: 100%;
+`;
+
+export const HeaderLinkLogo = styled.a`
+  height: 100%;
+  color: white;
+
+  gap: 8px;
+  display: flex;
+  align-items: center;
+
+  cursor: pointer;
+  text-decoration: none;
+`;
+
+export const HeaderLogotipo = styled.img`
+  height: 100%;
+`;
+
+export const HeaderTitle = styled.h1`
+  font-size: 1.25rem;
+  font-weight: 500;
+`;
+
+export const HeaderContainerButtons = styled.div`
+  gap: 8px;
+  display: flex;
+  align-items: center;
 `;
