ccd3010007a35dd39c52f7dcd1ca9d0f379f1116
victorgpd <victorguilherme2407@outlook.com>
2025-06-10 20:40:46 -0300

    inicialização do firebase

---
+++ src/firebase/config.ts
@@ -0,0 +1,13 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBPt67mVyIM0GDSLhm8U6XD4T7-HxLd4gU",
+  authDomain: "ressys-19d4b.firebaseapp.com",
+  projectId: "ressys-19d4b",
+  storageBucket: "ressys-19d4b.firebasestorage.app",
+  messagingSenderId: "789691016676",
+  appId: "1:789691016676:web:76665008fadb81939c93f1",
+};
+
+export const app = initializeApp(firebaseConfig);
